120cb2106897d61d60fef8aea4d400d0e0ad88e5
Ben Crist <ben@magicmoremagic.com>
2023-05-21 17:28:18 -0500

    test files

--- bootfib.asm
+++ bootfib.asm
@@ -1,5 +1,15 @@
-.boot
-    .org 0x10
+    .boot vector_table
+    .org 0
+_double_fault:                  .dw (.raw @halt)'16
+_page_fault:                    .dw (.raw @halt)'16
+_access_fault:                  .dw (.raw @halt)'16
+_page_align_fault:              .dw (.raw @halt)'16
+_instruction_protection_fault:  .dw (.raw @halt)'16
+_invalid_instruction:           .dw (.raw @halt)'16
+_pipe_0_reset:                  .dw (.raw @reset)'16
+
+    .boot
+reset:
     .def n r0
     .def last r1
     .def prev r2
@@ -11,3 +21,7 @@ loop:
     add last, prev -> last
     c temp -> prev
     b   loop
+
+halt:
+    park
+    b halt
